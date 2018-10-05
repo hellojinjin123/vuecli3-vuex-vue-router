45a26aff07755d82b9102924c997203e3bb8ebac
jmh <496528759@qq.com>
2018-10-05 09:38:26 +0800

    build project

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
+// mint-ui
+import { Header, Swipe, SwipeItem, Button } from 'mint-ui'
+Vue.component(Header.name, Header);
+Vue.component(Swipe.name, Swipe)
+
+import App from './App.vue'
+
 new Vue({
+  el: '#app',
   router,
-  store,
+  // store,
   render: h => h(App)
-}).$mount('#app')
+})
