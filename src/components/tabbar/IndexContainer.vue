40a4319fe7f5314ac7a266712bd5f3ed9f215750
jmh <496528759@qq.com>
2018-10-07 13:21:11 +0800

    修改

---
+++ src/components/tabbar/IndexContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="index-container">
+    <List></List>
+  </div>
+</template>
+
+<script>
+  import List from '../news/List'
+  export default {
+    data() {
+      return{}
+    },
+    components: {
+      List
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .index-container{
+    padding-bottom: r(63);
+  }
+</style>
